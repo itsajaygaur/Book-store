f8468e3fec43c4dc24662574ef474d54f8c6400f
Ajay <ajaypathak2527@gmail.com>
2022-06-02 22:02:31 +0530

    fixed

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const router = require("./routes/book-routes");
 const cors = require("cors");
 const app = express();
+const port = process.env.PORT
 require('dotenv').config()
 // Middlewares
 app.use(express.json());
@@ -10,14 +11,14 @@ app.use(cors());
 app.use("/books", router); // localhost:5000/books
 
 //route
-// app.get('/', (req, res) => {
-//   res.send('WORKING')
-// })
+app.get('/', (req, res) => {
+  res.send('WORKING')
+})
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("Connected To Database"))
   .then(() => {
-    app.listen(5000);
+    app.listen(port);
   })
   .catch((err) => console.log(err));
