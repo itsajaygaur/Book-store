eb7b6109792f86237e3522361eabfde69a77321e
Ajay <ajaypathak2527@gmail.com>
2022-06-03 20:00:25 +0530

    bakcend fixes

--- src/components/Book/Books.js
+++ src/components/Book/Books.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./Book.css";
 import axios from "axios";
 import Book from "./Book";
-const URL = "https://book-store-clone.herokuapp.com/";
+const URL = "https://book-store-clone.herokuapp.com/books/";
 const fetchHandler = async () => {
   return await axios.get(URL).then((res) => res.data);
 };
